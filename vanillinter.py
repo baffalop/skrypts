4c0d63e61811e7b6d55d3ffa1c4fcf881a27fafd
Nikita Gaidakov <ngaidakov@podfather.com>
2018-06-21 18:05:17 +0100

    Added ignore and verbose options

--- vanillinter.py
+++ vanillinter.py
@@ -1,14 +1,17 @@
 #!/usr/bin/env python3
 
 import os
+import os.path as pathTools
 import sys
 import re
 import argparse
 
 vanilla = {'bang','change','float','int','makefilename','moses','pack','print','receive','route','select','send','spigot','swap','symbol','trigger','unpack','until','value','choice','&','|','<<','>>','&&','||','%','+','-','*','/','pow','>','>=','=','<=','<','clip','max','min','mod','div','sin','cos','tan','atan','atan2','exp','log','abs','sqrt','pow','mtof','ftom','dbtorms','rmstodb','dbtopow','powtodb','random','wrap','expr','cputime','delay','line','metro','pipe','realtime','timer','makenote','notein','ctlin','pgmin','bendin','touchin','polytouchin','midiin','sysexin','noteout','ctlout','pgmout','bendout','touchout','polytouchout','midiout','stripnote','tabread','tabread4','tabwrite','soundfiler','loadbang','serial','netsend','netreceive','qlist','textfile','openpanel','savepanel','bag','poly','key','keyup','keyname','declare','adc~','dac~','bang~','block~','switch~','catch~','throw~','line~','vline~','threshold~','snapshot~','vsnapshot~','samplerate~','readsf~','receive~','send~','writesf~','sig~','+~','-~','*~','/~','max~','min~','clip~','q8_rsqrt~','q8_sqrt~','wrap~','fft~','ifft~','rfft~','rifft~','framp~','mtof~','ftom~','rmstodb~','dbtorms~','rmstopow~','powtorms~','pow~','log~','exp~','abs~','expr~','fexpr~','phasor~','cos~','osc~','tabwrite~','tabplay~','tabread~','tabread4~','tabosc4~','tabsend~','tabreceive~','bonk~','fiddle~','env~','vcf~','noise~','hip~','lop~','bp~','biquad~','samphold~','print~','rpole~','rzero~','rzero_rev~','cpole~','czero~','czero_rev~','complex-mod~','hilbert~','rev1~','rev2~','rev3~','delwrite~','delread~','vd~','pd','inlet','outlet','inlet~','outlet~','table','drawcurve','filledcurve','drawpolygon','filledpolygon','plot','drawnumber','struct','pointer','get','set','element','getsize','setsize','append','sublist','t','f','until','list','s','hsl','nbx','length','del','tgl','r','bng','sel','gate','i','r~','s~'}
 
-def lint(path, abstractions, extended=None):
+def lint(path, abstractions, extended=None, verbose=False):
     global vanilla
+    if verbose:
+        print('Linting ' + path)
     with open(path, 'r') as pdFile:
         canvasStack = [None]
         
@@ -38,7 +41,7 @@ def pickTheLint(object, line, path, subpatch=None, extended=None):
     extendedMessage = ''
     if extended != None:
         if object in extended:
-            libraryName = os.path.split(extended[object])[0].split('/')[-1]
+            libraryName = pathTools.split(extended[object])[0].split('/')[-1]
             extendedMessage = ' (%s)' % libraryName
         else:
             extendedMessage = ' (not found in extended)'
@@ -46,20 +49,30 @@ def pickTheLint(object, line, path, subpatch=None, extended=None):
     message = 'Line {}{} of {}: {}{}'.format(line, subpatchMessage, path, object, extendedMessage)
     print(message)
 
-def recursePath(path):
+def recursePath(path, verbose=False, ignore={}):
     '''Build a dictionary of {filename: path}s, scanning the given path recursively'''
-    tail = os.path.split(path)[1]
+    tail = pathTools.split(path)[1]
     if len(tail) and tail[0] == '.':
         return {}
-    if os.path.isfile(path):
-        filename, extension = os.path.splitext(tail)
+    if pathTools.isfile(path):
+        filename, extension = pathTools.splitext(tail)
         if extension == '.pd' or extension == '.pd_linux':
+            if verbose:
+                print('Found pd file ' + filename + extension)
             return {filename: path}
-    if os.path.isdir(path):
+    if pathTools.isdir(path):
+        if pathTools.abspath(path) in ignore:
+            if verbose:
+                print('Ignoring ' + path)
+            return {}
+        if verbose:
+            print('Preparing directory ' + path)
         paths = {}
         for subfile in os.listdir(path):
-            subpath = os.path.join(path, subfile)
-            paths = {**paths, **recursePath(subpath)}
+            subpath = pathTools.join(path, subfile)
+            paths = {**paths, **recursePath(subpath, verbose, ignore)}
+        if verbose:
+            print('Done with directory ' + path)
         return paths
     return {}
 
@@ -70,18 +83,24 @@ def main(args):
         for path in args.e:
             extended = {**extended, **recursePath(path[0])}
     
+    ignore = {}
+    if args.i != None:
+        ignore = { pathTools.abspath(ilist[0]) for ilist in args.i }
+    
     pathdict = {}
     for path in args.paths:
-        pathdict = {**pathdict, **recursePath(path)}
+        pathdict = {**pathdict, **recursePath(path, args.verbose, ignore)}
     
     for _, path in pathdict.items():
-        lint(path, pathdict, extended)
+        lint(path, pathdict, extended, args.verbose)
 
 if __name__ == '__main__':
     eHelp = 'Path to Pd-extended or similar collection of libraries. The linter will tell you which library each linted object comes from.'
     pathHelp = 'Pd files or directories to scan. If none are given, current working directory is scanned. Directories are scanned recursively.'
     argParser = argparse.ArgumentParser(description='Scan pd files for non-vanilla objects.')
     argParser.add_argument('-e', nargs=1, metavar='PATH', action='append', help=eHelp)
+    argParser.add_argument('-i', nargs=1, metavar='PATH', action='append', help='Directory to ignore when scanning recursively')
+    argParser.add_argument('--verbose', '-v', action='store_true')
     argParser.add_argument('paths', nargs='*', metavar='PATH', default=[os.getcwd()], help=pathHelp)
     
     args = argParser.parse_args()
